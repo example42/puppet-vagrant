a8b9821e09145281d52ca452d83eb6c45199bb87
Alessandro Franceschi <al@lab42.it>
2012-08-26 12:46:42 +0200

    Tuning vagrant::vm define

--- manifests/vm.pp
+++ manifests/vm.pp
@@ -16,32 +16,63 @@
 #
 #
 define vagrant::vm (
-    $environment,
-    $box="base",
-    $box_url="",
-    $forward_name="",
-    $forward_hostport="",
-    $forward_guestport="",
-    $share_name="",
-    $share_hostdir="",
-    $share_guestmount="",
-    $network="",
-    $boot_mode="",
-    $provision="",
-    $puppet_server="$puppet_server",
-    $puppet_node="$fqdn",
-    $chef_orgname="",
-    $order="50",
-    $ensure="present" ) {
+  $environment,
+  $box                  = 'base',
+  $box_url              = '',
+  $network              = '',
+  $ip_address           = '',
+  $forward_hostport     = '',
+  $forward_guestport    = '',
+  $share_name           = '',
+  $share_hostdir        = '',
+  $share_guestmount     = '',
+  $boot_mode            = '',
+  $provision            = '',
+  $puppet_server        = 'puppet',
+  $puppet_node          = $::fqdn,
+  $puppet_manifest_path = 'manifests',
+  $puppet_manifest_file = 'site.pp',
+  $puppet_module_path   = '',
+  $puppet_options       = '',
+  $chef_server          = '',
+  $chef_cookbooks_path  = '',
+  $shell_path           = '',
+  $order                = '50',
+  $ensure               = 'present' ) {
 
-    include vagrant::params
-    include concat::setup
+  include vagrant
+  include concat::setup
 
-    concat::fragment{ "Vagrantfile_vm_$environment_$name":
-        target  => "${vagrant::params::basedir}/${environment}/Vagrantfile",
-        content => template("vagrant/concat/Vagrantfile_vm.erb"),
-        order   => $order,
-        ensure  => $ensure,
+  concat::fragment{ "Vagrantfile_vm_$environment_$name":
+    target  => "${vagrant::basedir}/${environment}/Vagrantfile",
+    content => template("vagrant/concat/Vagrantfile_vm.erb"),
+    order   => $order,
+    ensure  => $ensure,
+  }
+
+  ### Service monitoring, if enabled ( monitor => true )
+  if $vagrant::bool_monitor == true {
+    monitor::port { "vagrant_${vagrant::protocol}_${forward_hostport}":
+      protocol => $vagrant::protocol,
+      port     => $forward_hostport,
+      target   => $vagrant::monitor_target,
+      tool     => $vagrant::monitor_tool,
+      enable   => $vagrant::manage_monitor,
     }
+  }
+
+  ### Firewall management, if enabled ( firewall => true )
+  if $vagrant::bool_firewall == true {
+    firewall { "vagrant_${vagrant::protocol}_${forward_hostport}":
+      source      => $vagrant::firewall_src,
+      destination => $vagrant::firewall_dst,
+      protocol    => $vagrant::protocol,
+      port        => $forward_hostport,
+      action      => 'allow',
+      direction   => 'input',
+      tool        => $vagrant::firewall_tool,
+      enable      => $vagrant::manage_firewall,
+    }
+  }
 
 }
