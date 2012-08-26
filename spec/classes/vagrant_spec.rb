855a0b54be5fb4da0b302edbde2df446d59c262c
Alessandro Franceschi <al@lab42.it>
2012-08-26 11:47:28 +0200

    Fixes spec tests

--- spec/classes/vagrant_spec.rb
+++ spec/classes/vagrant_spec.rb
@@ -8,8 +8,6 @@ describe 'vagrant' do
 
   describe 'Test standard installation' do
     it { should contain_package('vagrant').with_ensure('present') }
-    it { should contain_service('vagrant').with_ensure('running') }
-    it { should contain_service('vagrant').with_enable('true') }
     it { should contain_file('vagrant.conf').with_ensure('present') }
   end
 
@@ -18,73 +16,24 @@ describe 'vagrant' do
     it { should contain_package('vagrant').with_ensure('1.0.42') }
   end
 
-  describe 'Test standard installation with monitoring and firewalling' do
-    let(:params) { {:monitor => true , :firewall => true, :port => '42', :protocol => 'tcp' } }
-
-    it { should contain_package('vagrant').with_ensure('present') }
-    it { should contain_service('vagrant').with_ensure('running') }
-    it { should contain_service('vagrant').with_enable('true') }
-    it { should contain_file('vagrant.conf').with_ensure('present') }
-    it 'should monitor the process' do
-      content = catalogue.resource('monitor::process', 'vagrant_process').send(:parameters)[:enable]
-      content.should == true
-    end
-    it 'should place a firewall rule' do
-      content = catalogue.resource('firewall', 'vagrant_tcp_42').send(:parameters)[:enable]
-      content.should == true
-    end
-  end
-
   describe 'Test decommissioning - absent' do
-    let(:params) { {:absent => true, :monitor => true , :firewall => true, :port => '42', :protocol => 'tcp'} }
+    let(:params) { {:absent => true, :monitor => true , :firewall => true} }
 
     it 'should remove Package[vagrant]' do should contain_package('vagrant').with_ensure('absent') end 
-    it 'should stop Service[vagrant]' do should contain_service('vagrant').with_ensure('stopped') end
-    it 'should not enable at boot Service[vagrant]' do should contain_service('vagrant').with_enable('false') end
     it 'should remove vagrant configuration file' do should contain_file('vagrant.conf').with_ensure('absent') end
-    it 'should not monitor the process' do
-      content = catalogue.resource('monitor::process', 'vagrant_process').send(:parameters)[:enable]
-      content.should == false
-    end
-    it 'should remove a firewall rule' do
-      content = catalogue.resource('firewall', 'vagrant_tcp_42').send(:parameters)[:enable]
-      content.should == false
-    end
   end
 
   describe 'Test decommissioning - disable' do
-    let(:params) { {:disable => true, :monitor => true , :firewall => true, :port => '42', :protocol => 'tcp'} }
+    let(:params) { {:disable => true, :monitor => true , :firewall => true } }
 
     it { should contain_package('vagrant').with_ensure('present') }
-    it 'should stop Service[vagrant]' do should contain_service('vagrant').with_ensure('stopped') end
-    it 'should not enable at boot Service[vagrant]' do should contain_service('vagrant').with_enable('false') end
     it { should contain_file('vagrant.conf').with_ensure('present') }
-    it 'should not monitor the process' do
-      content = catalogue.resource('monitor::process', 'vagrant_process').send(:parameters)[:enable]
-      content.should == false
-    end
-    it 'should remove a firewall rule' do
-      content = catalogue.resource('firewall', 'vagrant_tcp_42').send(:parameters)[:enable]
-      content.should == false
-    end
   end
 
   describe 'Test decommissioning - disableboot' do
-    let(:params) { {:disableboot => true, :monitor => true , :firewall => true, :port => '42', :protocol => 'tcp'} }
+    let(:params) { {:disableboot => true, :monitor => true , :firewall => true} }
   
     it { should contain_package('vagrant').with_ensure('present') }
-    it { should_not contain_service('vagrant').with_ensure('present') }
-    it { should_not contain_service('vagrant').with_ensure('absent') }
-    it 'should not enable at boot Service[vagrant]' do should contain_service('vagrant').with_enable('false') end
-    it { should contain_file('vagrant.conf').with_ensure('present') }
-    it 'should not monitor the process locally' do
-      content = catalogue.resource('monitor::process', 'vagrant_process').send(:parameters)[:enable]
-      content.should == false
-    end
-    it 'should keep a firewall rule' do
-      content = catalogue.resource('firewall', 'vagrant_tcp_42').send(:parameters)[:enable]
-      content.should == true
-    end
   end 
 
   describe 'Test customizations - template' do
@@ -126,103 +75,12 @@ describe 'vagrant' do
     end
   end
 
-  describe 'Test service autorestart', :broken => true do
-    it 'should automatically restart the service, by default' do
-      content = catalogue.resource('file', 'vagrant.conf').send(:parameters)[:notify]
-      content.should == 'Service[vagrant]{:name=>"vagrant"}'
-    end
-  end
-
-  describe 'Test service autorestart' do
-    let(:params) { {:service_autorestart => "no" } }
-
-    it 'should not automatically restart the service, when service_autorestart => false' do
-      content = catalogue.resource('file', 'vagrant.conf').send(:parameters)[:notify]
-      content.should be_nil
-    end
-  end
-
   describe 'Test Puppi Integration' do
     let(:params) { {:puppi => true, :puppi_helper => "myhelper"} }
 
     it 'should generate a puppi::ze define' do
       content = catalogue.resource('puppi::ze', 'vagrant').send(:parameters)[:helper]
       content.should == "myhelper"
-    end
-  end
-
-  describe 'Test Monitoring Tools Integration' do
-    let(:params) { {:monitor => true, :monitor_tool => "puppi" } }
-
-    it 'should generate monitor defines' do
-      content = catalogue.resource('monitor::process', 'vagrant_process').send(:parameters)[:tool]
-      content.should == "puppi"
-    end
-  end
-
-  describe 'Test Firewall Tools Integration' do
-    let(:params) { {:firewall => true, :firewall_tool => "iptables" , :protocol => "tcp" , :port => "42" } }
-
-    it 'should generate correct firewall define' do
-      content = catalogue.resource('firewall', 'vagrant_tcp_42').send(:parameters)[:tool]
-      content.should == "iptables"
-    end
-  end
-
-  describe 'Test OldGen Module Set Integration' do
-    let(:params) { {:monitor => "yes" , :monitor_tool => "puppi" , :firewall => "yes" , :firewall_tool => "iptables" , :puppi => "yes" , :port => "42" , :protocol => 'tcp' } }
-
-    it 'should generate monitor resources' do
-      content = catalogue.resource('monitor::process', 'vagrant_process').send(:parameters)[:tool]
-      content.should == "puppi"
-    end
-    it 'should generate firewall resources' do
-      content = catalogue.resource('firewall', 'vagrant_tcp_42').send(:parameters)[:tool]
-      content.should == "iptables"
-    end
-    it 'should generate puppi resources ' do 
-      content = catalogue.resource('puppi::ze', 'vagrant').send(:parameters)[:ensure]
-      content.should == "present"
-    end
-  end
-
-  describe 'Test params lookup' do
-    let(:facts) { { :monitor => true , :ipaddress => '10.42.42.42' } }
-    let(:params) { { :port => '42' } }
-
-    it 'should honour top scope global vars' do
-      content = catalogue.resource('monitor::process', 'vagrant_process').send(:parameters)[:enable]
-      content.should == true
-    end
-  end
-
-  describe 'Test params lookup' do
-    let(:facts) { { :vagrant_monitor => true , :ipaddress => '10.42.42.42' } }
-    let(:params) { { :port => '42' } }
-
-    it 'should honour module specific vars' do
-      content = catalogue.resource('monitor::process', 'vagrant_process').send(:parameters)[:enable]
-      content.should == true
-    end
-  end
-
-  describe 'Test params lookup' do
-    let(:facts) { { :monitor => false , :vagrant_monitor => true , :ipaddress => '10.42.42.42' } }
-    let(:params) { { :port => '42' } }
-
-    it 'should honour top scope module specific over global vars' do
-      content = catalogue.resource('monitor::process', 'vagrant_process').send(:parameters)[:enable]
-      content.should == true
-    end
-  end
-
-  describe 'Test params lookup' do
-    let(:facts) { { :monitor => false , :ipaddress => '10.42.42.42' } }
-    let(:params) { { :monitor => true , :firewall => true, :port => '42' } }
-
-    it 'should honour passed params over global vars' do
-      content = catalogue.resource('monitor::process', 'vagrant_process').send(:parameters)[:enable]
-      content.should == true
     end
   end
 
